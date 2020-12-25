51c2ec1c9fd678c48bb26f9bf5c34d4c9b83e995
omer lev <omerlev66@gmail.com>
2020-12-25 03:26:59 +0200

    init

---
+++ Assets/scripts/hero.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class hero : MonoBehaviour
+{
+    public float vel = 3;
+
+    public Rigidbody2D rb;
+
+    Vector2 input;
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + input * vel * Time.deltaTime);
+    }
+}
