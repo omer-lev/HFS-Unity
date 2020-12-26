aa1af48ab6c7b0d5abcfe65d175d56d6ecaf5d35
Nadav <nadav.gabriel@gmail.com>
2020-12-27 00:32:40 +0200

    New save and read system (needs fixing)

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,47 +1,82 @@
 ﻿using UnityEngine;
 using System.IO;
 
+
 public class GameManager : MonoBehaviour
 {
-    string path;
-    public GameObject Player;
-    bool clearSave = false;
+    Save save;
+    public GameObject player;
 
     void Start()
     {
-        path = Application.dataPath + "/game.save";
-
-        Load();
-    }
-
-    private void ClearSave()
-    {
-        File.WriteAllText(path, "");
-        clearSave = true;
+        save.LoadFile();
+        save.Init(player);
     }
 
     void Update()
     {
-        if(Input.GetKeyDown("r"))
+        if(Input.GetKeyDown(KeyCode.Delete))
         {
-            ClearSave();
+            save.ClearSave();
         }
     }
 
     private void OnApplicationQuit()
     {
-        if (!clearSave)
+        if (!save.clearSave)
         {
-            Save();
+            save.SaveFile();
+        }
+    }
+}
+
+public class Save : MonoBehaviour
+{
+    GameObject Player;
+    public string path;
+    public bool clearSave;
+
+    /// <summary>
+    /// Used to initialize the components used inside this class
+    /// </summary>
+    /// <param name="go">GameObject of the object you want to save its position</param>
+    public void Init(GameObject go)
+    {
+        path = Application.dataPath + "/game.save";
+        clearSave = false;
+        Player = go;
+    }
+
+    /// <summary>
+    /// Deletes any data supplied to path
+    /// </summary>
+    public void ClearSave()
+    {
+        File.WriteAllText(path, "");
+        clearSave = true;
+    }
+
+    /// <summary>
+    /// Self explanetory...
+    /// </summary>
+    public void SaveFile()
+    {
+        try
+        {
+            print("Saving data to file");
+            File.WriteAllText(path, Player.transform.position.ToString());
+            print("Saving done");
+        }
+        catch (System.Exception exep)
+        {
+            print("Error loading file: " + exep.Message + " [in Save() function]");
         }
     }
 
-    private void Save()
-    {
-        File.WriteAllText(path, Player.transform.position.ToString());
-    }
-
-    private void Load()
+    /// <summary>
+    /// Self explanetory...
+    /// </summary>
+    public void LoadFile()
     {
         try
         {
@@ -54,10 +89,31 @@ public class GameManager : MonoBehaviour
             posY = float.Parse(posString[2]);
 
             Player.transform.position = new Vector3(posX, posY, 0);
+            print("Reading done");
         }
         catch (System.Exception exep)
         {
-            print("No data in game.save: " + exep.Message);
+            print("Error loading file: " + exep.Message);
         }
+    }
+
+    /// <summary>
+    /// Self explanetory...
+    /// </summary>
+    public void Encript()
+    {
+        print("Encripting save file");
+        File.Encrypt(path);
+        print("Encripting done");
+    }
+
+    /// <summary>
+    /// Self explanetory...
+    /// </summary>
+    public void Decript()
+    {
+        print("Decripting save file");
+        File.Decrypt(path);
+        print("Decripting done, reading file...");
     }
 }
