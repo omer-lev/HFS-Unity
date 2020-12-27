608b0349877d5826a51996d8769daa246488c517
omer lev <omerlev66@gmail.com>
2020-12-27 23:52:42 +0200

    fixed position saving and loading, removed commented out unnecessery class.

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -4,78 +4,50 @@ using System.IO;
 
 public class GameManager : MonoBehaviour
 {
-    Save save;
-    public GameObject player;
+    public GameObject Player;
+    string path;
+    bool clearSave = false;
 
     void Start()
     {
-        save.LoadFile();
-        save.Init(player);
+        path = Application.dataPath + "/game.save";
+        LoadFile();
     }
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Delete))
         {
-            save.ClearSave();
+            ClearSave();
         }
     }
 
     private void OnApplicationQuit()
     {
-        if (!save.clearSave)
+        if (!clearSave)
         {
-            save.SaveFile();
+            SaveFile();
         }
     }
-}
 
-public class Save : MonoBehaviour
-{
-    GameObject Player;
-    public string path;
-    public bool clearSave;
-
-    /// <summary>
-    /// Used to initialize the components used inside this class
-    /// </summary>
-    /// <param name="go">GameObject of the object you want to save its position</param>
-    public void Init(GameObject go)
-    {
-        path = Application.dataPath + "/game.save";
-        clearSave = false;
-        Player = go;
-    }
-
-    /// <summary>
-    /// Deletes any data supplied to path
-    /// </summary>
     public void ClearSave()
     {
         File.WriteAllText(path, "");
         clearSave = true;
     }
 
-    /// <summary>
-    /// Self explanetory...
-    /// </summary>
     public void SaveFile()
     {
         try
         {
-            print("Saving data to file");
             File.WriteAllText(path, Player.transform.position.ToString());
-            print("Saving done");
         }
         catch (System.Exception exep)
         {
-            print("Error loading file: " + exep.Message + " [in Save() function]");
+            print("Error while saving to game.save file: " + exep.Message + " [in Save() function]");
         }
     }
 
-    /// <summary>
-    /// Self explanetory...
-    /// </summary>
     public void LoadFile()
     {
         try
@@ -89,31 +61,40 @@ public class Save : MonoBehaviour
             posY = float.Parse(posString[2]);
 
             Player.transform.position = new Vector3(posX, posY, 0);
-            print("Reading done");
         }
         catch (System.Exception exep)
         {
             print("Error loading file: " + exep.Message);
         }
     }
-
-    /// <summary>
-    /// Self explanetory...
-    /// </summary>
-    public void Encript()
-    {
-        print("Encripting save file");
-        File.Encrypt(path);
-        print("Encripting done");
-    }
-
-    /// <summary>
-    /// Self explanetory...
-    /// </summary>
-    public void Decript()
-    {
-        print("Decripting save file");
-        File.Decrypt(path);
-        print("Decripting done, reading file...");
-    }
 }
+
+//public class Save : MonoBehaviour
+//{
+    
+
+
+    /// <param name="go">GameObject of the object you want to save its position</param>
+    // public void Init(GameObject go)
+    // {
+    //     path = Application.dataPath + "/game.save";
+    //     clearSave = false;
+    //     Player = go;
+    // }
+
+    
+
+    // public void Encript()
+    // {
+    //     print("Encripting save file");
+    //     File.Encrypt(path);
+    //     print("Encripting done");
+    // }
+    // 
+    // public void Decript()
+    // {
+    //     print("Decripting save file");
+    //     File.Decrypt(path);
+    //     print("Decripting done, reading file...");
+    // }
+//}
